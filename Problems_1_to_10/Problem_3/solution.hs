ff902a1e8fa5ffed1bd536ef6edc435270a34f4a
wsrtka <witold.serwatka@pm.me>
2020-12-23 19:37:24 +0100

    added solution to problem 3

--- Problems_1_to_10/Problem_3/solution.hs
+++ Problems_1_to_10/Problem_3/solution.hs
@@ -1,3 +1,11 @@
 --Solution using indexing
 elementAt :: [a] -> Int -> a
 elementAt x i = x !! (i - 1)
+
+--Interesting solution from wiki
+elementAt' :: [a] -> Int -> a
+elementAt' (x:_) 1 = x                              --define edge condition
+elementAt' [] _    = error "Index out of bounds"    --if list is empty, error regardless of second argument
+elementAt' (_:xs) k
+    | k < 1        = error "Index out of bounds"    --error if second argument is negative
+    | otherwise    = elementAt' xs (k - 1)          --otherwise is always equal to True, recursive version
