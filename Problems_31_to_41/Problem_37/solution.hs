1af86bac44a9022236dc65994ce48e11a0e96841
wsrtka <witold.serwatka@pm.me>
2021-01-18 14:24:57 +0100

    added solution to problem 37

---
+++ Problems_31_to_41/Problem_37/solution.hs
@@ -0,0 +1,25 @@
+--solution from problem 9
+pack :: Eq a => [a] -> [[a]]
+pack [] = []
+pack (x:xs) = let (first, rest) = span (==x) xs
+              in (x : first) : pack rest
+
+--solution from problem 10
+encode :: Eq a => [a] -> [(Int, a)]
+encode x = let packed = pack x
+               f y    = (length y, head y)
+           in map f packed
+
+--solution from problem 35
+primeFactors :: Int -> [Int]
+primeFactors 1 = []
+primeFactors n = x : primeFactors (n `div` x)
+                 where x = head [i | i <- [2..n], n `mod` i == 0]
+
+--solution form problem 36
+primeFactorsMult :: Int -> [(Int, Int)]
+primeFactorsMult n = encode $ primeFactors n
+
+--solution for problem 37
+totient :: Int -> Int 
+totient m = product [(p - 1) * p ^ (c - 1) | (p, c) <- primeFactorsMult m]
